881904276cbbf35fb1c2cf59d93027fe35184d4c
Saltyhuanzi <931168727@qq.com>
2024-12-20 21:38:22 +0800

    对于物件文本和相关文件的上传

---
+++ Thebirthdayparty/Assets/Scripts/InteractiveObjects/Instances/A06/Script.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using QFramework;
+using UnityEngine;
+using Interactable;
+namespace A06
+{
+    public class Script : MonoBehaviour
+    {
+        private void Start()
+        {
+            StartCoroutine(IERunning());
+        }
+        IEnumerator IERunning()
+        {
+            TypeEventSystem.Global.Send<InteractStart>();
+            Debug.Log("Matt's napkin.");
+            yield return new WaitForSeconds(3);
+            Debug.Log("......");
+            TypeEventSystem.Global.Send<NextPeriodEvent>();
+            TypeEventSystem.Global.Send<RequireSaveGame>();
+            TypeEventSystem.Global.Send<InteractEnd>();
+        }
+    }
+}
